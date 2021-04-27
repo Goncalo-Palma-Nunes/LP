78e4e58fa44f1537e4871ffc2616c278dd725f4f
Goncalo-Palma-Nunes <73646319+Goncalo-Palma-Nunes@users.noreply.github.com>
2021-04-27 21:17:50 +0100

    Added permutacoes_soma
    The predicate permutacoes_soma(N, Els, Soma, Perms) shows a list of all possible permutations of the N by N combinations of Els whose sum equals Soma.

--- Project.pl
+++ Project.pl
@@ -15,3 +15,16 @@ combinacoes_soma(N, Els, Soma, Combs) :-
      findall(X,
              (combinacao(N, Els, X), sum_list(X, Sum),
               Sum =:= Soma), Combs).
+
+
+% -----------------------------------------------------------------------
+%                  permutacoes_soma(N, Els, Soma, Perms)
+% permutacoes_soma(N, Els, Soma, Perms), em que N eh um inteiro, Els eh
+% uma lista de inteiros, e Soma eh um inteiro, significa que Perms eh a
+% lista ordenada cujos elementos sao as permutações das combinações N a
+% N, dos elementos de Els cuja soma eh Soma.
+% -----------------------------------------------------------------------
+permutacoes_soma(N, Els, Soma, Perms) :-
+     combinacoes_soma(N, Els, Soma, Combs),
+     findall(X, (member(Y, Combs), permutation(Y, X)), Permutado),
+     sort(Permutado,Perms).
